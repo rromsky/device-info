b21d1b15c9b60c4431e75a2421f2e644a868cf64
Молодец <@DESKTOP-67AA7AR>
2023-02-01 15:15:04 +0200

    Добавьте файлы проекта.

---
+++ ND/ND.cpp
@@ -0,0 +1,282 @@
+﻿// ND.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "ND.h"
+#include <Windows.h>
+#include <SetupAPI.h>
+#include <devguid.h>
+#include <cfgmgr32.h>
+#include <iostream>
+#include <fstream>
+#include <atlbase.h>
+
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "oleaut32.lib")
+#pragma comment(lib, "comsupp.lib")
+#pragma comment(lib, "comsuppw.lib")
+#pragma comment(lib, "strmiids.lib")
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+
+
+class DevS {          // поток для записи
+public:
+    int main()
+    {
+
+        std::ofstream out;
+        out.open("device.txt"); // окрываем файл для записи
+        if (out.is_open())
+        {
+            out << "Info about devices:" << std::endl;
+        }
+        setlocale(LC_ALL, "");
+        HDEVINFO hDevInfo;
+        SP_DEVINFO_DATA DeviceInfoData;
+        DWORD i;
+
+        // Create a HDEVINFO with all present devices.
+        hDevInfo = SetupDiGetClassDevs(&GUID_DEVCLASS_USB, 0, 0, DIGCF_PRESENT | DIGCF_ALLCLASSES);
+        if (hDevInfo == INVALID_HANDLE_VALUE)
+        {
+            MessageBoxA(NULL, "Error in SetupDiGetClassDevs", "Error", NULL);
+            return 1;
+        }
+
+        // Enumerate through all devices in the HDEVINFO list.
+        DeviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+        for (i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &DeviceInfoData); i++)
+        {
+            DWORD DataT;
+            LPTSTR buffer = NULL;
+            DWORD buffersize = 0;
+
+            //
+            // Call function with null to begin with, 
+            // then use the returned buffer size (doubled)
+            // to Alloc the buffer. Keep calling until
+            // success or an unknown failure.
+            //
+            while (!SetupDiGetDeviceRegistryProperty(
+                hDevInfo,
+                &DeviceInfoData,
+                SPDRP_DEVICEDESC,
+                &DataT,
+                (PBYTE)buffer,
+                buffersize,
+                &buffersize))
+            {
+                if (GetLastError() == ERROR_INSUFFICIENT_BUFFER)
+                {
+                    // Change the buffer size.
+                    if (buffer) LocalFree(buffer);
+                    buffer = (LPTSTR)LocalAlloc(LPTR, buffersize * 2);
+                }
+                else
+                {
+                    // Insert error handling here.
+                    break;
+                }
+            }
+
+            int size = WideCharToMultiByte(CP_ACP, WC_COMPOSITECHECK, buffer, -1, NULL, 0, NULL, NULL);
+            char* mb_buffer = new char[size];
+            WideCharToMultiByte(CP_ACP, WC_COMPOSITECHECK, buffer, -1, mb_buffer, size, NULL, NULL);
+
+            out << "Пристрій: " << mb_buffer << std::endl;
+            if (buffer) LocalFree(buffer);
+            if (mb_buffer) delete[] mb_buffer;
+        }
+
+        if (GetLastError() != NO_ERROR && GetLastError() != ERROR_NO_MORE_ITEMS)
+        {
+            // Insert error handling here.
+            return 1;
+        }
+
+        // Cleanup
+        SetupDiDestroyDeviceInfoList(hDevInfo);
+
+        return 0;
+    }
+};
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_ND, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ND));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ND));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_ND);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, 400,300, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            case 32771:
+                DevS getD;
+                getD.main();
+                WinExec("notepad.exe device.txt", SW_SHOW);
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
